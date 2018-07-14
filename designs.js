17a53ac2f5c157f286154eb107bd3c37274f01fe
Ben Pinder <benpinder91@gmail.com>
2018-07-14 11:46:08 +0100

    First Commit

---
+++ designs.js
@@ -0,0 +1,36 @@
+// Select color input
+// Select size input
+
+// When size is submitted by the user, call makeGrid()
+//get rows and get columns
+let rows = $("#inputHeight").val();
+let columns = $("#inputWidth").val();
+//get table
+const table = $("#pixelCanvas");
+// reset table
+   table.children().remove();
+//make grid function
+function makeGrid() {
+//create rows
+for (rows = 0; rows <= n; rows++){
+    table.append("<tr></tr>");
+//create columns
+    for (columns = 0; columns <= m; columns++){
+        table.children().last().append("<td></td>");
+    }
+ }
+//listen for clicks on cell
+table.on("click", "td", function()
+{
+//get colour
+    let color=$("input[type='color']").val();
+//apply color to cell
+    $(this).attr("bgcolor", color);
+ });
+}
+
+//listen for clicks on buttons to make grid
+$("input[type='submit']").click(function(e){
+    e.preventDefault();
+    makeGrid();
+});
